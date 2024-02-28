30ffef2fac815f022fa6794cd4d481340c9836a5
say <tpgml0816@gmail.com>
2024-02-28 17:21:48 +0900

    aws

---
+++ spring-server/Penpick_Run/src/main/java/com/penpick/PenpickRunApplication.java
@@ -0,0 +1,13 @@
+package com.penpick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PenpickRunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PenpickRunApplication.class, args);
+	}
+
+}
